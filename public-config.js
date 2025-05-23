61ca3fadfc274287d6bd6316de8a1cf5105ce1de
cbenedikter <claudio@onesignal.com>
2025-05-23 15:19:07 +0100

    Use public-config.js for credentials for GitHub Pages demo

---
+++ public-config.js
@@ -0,0 +1,5 @@
+// Public OneSignal API Configuration (keys are visible to all users)
+const config = {
+    APP_ID: "5e605fcd-de88-4b0a-a5eb-5c18b84d52f3",
+    API_KEY: "os_v2_app_lzqf7to6rbfqvjpllqmlqtks6p27gystfmiurmv2i5jbtkxlqrgwfi4whpacwylxa74wwuehxjh5wowj3rmajrrnjgcuxvsnqxf5yxq"
+}; 
